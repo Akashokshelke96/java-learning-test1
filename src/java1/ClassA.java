ffc8c04da504c5520c00489e9c50fb713798d2c2
Akash <aryanakashshelke@gmail.com>
2021-12-26 09:54:57 +0530

    Initial commit

---
+++ src/java1/ClassA.java
@@ -0,0 +1,11 @@
+package java1;
+
+public class ClassA {
+    public static void main(String[] args) {
+        for (int x=0;x<5;x++ ) {
+            System.out.println("i am java");
+
+        }
+
+    }
+}

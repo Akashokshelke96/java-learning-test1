cbc5c2d0b5c141e6c1a1281bd7ae460da52c5494
Akashokshelke96 <aryanakashshelke@gmail.com>
2021-12-28 13:52:31 +0530

    print number divisible by 2 or 3

---
+++ src/java1/ClassTopic3.java
@@ -0,0 +1,18 @@
+package java1;
+
+
+
+
+public class ClassTopic3 {
+
+    public static void main(String[] args) {
+        for (int n=1; n<=100;n++)
+        {
+           if((n%2==0) || (n%3==0)){
+               System.out.println(n);
+           }
+
+        }
+
+    }
+}

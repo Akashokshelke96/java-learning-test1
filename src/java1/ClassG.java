7f563b9a5424864cb3b3532b5e767767ca21efaf
Akashokshelke96 <aryanakashshelke@gmail.com>
2021-12-30 09:38:32 +0530

    right handed triangle startin on the left

---
+++ src/java1/ClassG.java
@@ -0,0 +1,19 @@
+package java1;
+
+public class ClassG {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = 1; i <= n; i++) {
+            int noOfSpaces = n - i;
+            for (int j = 1; j <= noOfSpaces; j++){
+                System.out.print(" ");
+            }
+            for (int j = 1 ; j <= i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+

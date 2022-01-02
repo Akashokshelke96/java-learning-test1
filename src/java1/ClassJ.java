0c0b906a4b11939999672d2b8758ace9fe0e5b4a
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-02 19:53:56 +0530

    A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
    Given a string , print Yes if it is a palindrome, print No otherwise.

---
+++ src/java1/ClassJ.java
@@ -0,0 +1,23 @@
+package java1;
+
+import java.util.Scanner;
+
+public class ClassJ {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        boolean flag = false;
+        for(int i=2;i<=num/2;++i) {
+            if (num% i ==0){
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+            System.out.println("Yes");
+        else {
+            System.out.println("NO");
+        }
+
+    }
+}

6c3c3349e6ed0f586ac1f766af0f855ead5b572b
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-03 10:49:07 +0530

    Inverted Left Traingle pattern with asterisk

---
+++ src/Patterns/MasterTriangle.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+public class MasterTriangle {
+        public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = n - 1; j >= i; j--) {
+                System.out.print(" "); // 0spaces= Left Align Trian. ; 1Space= Pyramid ; 2Spaces= Right Align Train.
+                                         //increasing the spaces will increase the angle and goes obtuse
+            }
+            for (int k = 1; k <= i; k++)
+            {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+    }
+    }

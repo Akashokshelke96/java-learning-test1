7ac6229f5330854944e44e7e29843e83e18cc8c5
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-12 13:55:29 +0530

    learning Classes functions

--- src/ClassesLearn/Main.java
+++ src/ClassesLearn/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Customer customer1 = new Customer("Akash", "Shelke", "241 - BakerStreet", "9399403930");
         Customer customer2 = new Customer("Nishu", "Rajoriya","232 - BakerStreet","9494475894" );
-//        System.out.println(customer1.getDetails());
-//        System.out.println(customer2.getDetails());
-//        System.out.println(customer1.getDial());
+        System.out.println(customer1.getDetails());
+        System.out.println(customer2.getDetails());
+        System.out.println(customer1.getDial());
         Car tata = new Car("BMW","SSW",2019 );
         System.out.println(tata.getPrice());
         Car AstonMartin = new Car("AstonMartin","SSR",2020);

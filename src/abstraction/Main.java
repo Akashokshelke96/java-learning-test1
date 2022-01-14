2e95581758c7e0b181435e576327ae7d546c136e
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-14 08:54:00 +0530

    learning abstracts functions; implements,interface

---
+++ src/abstraction/Main.java
@@ -0,0 +1,22 @@
+package abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+        DebitCard dc = new DebitCard();
+        Creditcard cc = new Creditcard();
+
+        System.out.println(getAvailableLimit(dc));
+        System.out.println((getAvailableLimit(cc)));
+        System.out.println(getSpentLimit(cc));
+
+    }
+
+    private static double getSpentLimit(Creditcard cc) {
+        return 3000;
+    }
+
+    public static double getAvailableLimit(Card card1) {
+        return card1.getTotalLimit() - card1.getSpentLimit();
+    }
+}
+

4035304db287114a81e9938231e07e1147ad53ef
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-17 16:53:56 +0530

    java  runtime polymorphism

--- src/polymorphism/Card.java
+++ src/polymorphism/Card.java
@@ -2,6 +2,6 @@ package polymorphism;
 
 public class Card {
     public void printType(){
-        System.out.println("i am a card");
+            System.out.println("i am a card");
+        }
     }
-}

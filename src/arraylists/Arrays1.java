cac60bf3cfb15e60d9d5d16aa872b07cfc8ea363
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-18 19:01:44 +0530

    java learning arrays from prepfortech

---
+++ src/arraylists/Arrays1.java
@@ -0,0 +1,21 @@
+package arraylists;
+
+import java.util.ArrayList;
+
+public class Arrays1 {
+    public static void main(String[] args) {
+        ArrayList<String> arr1 = new ArrayList<>();
+        arr1.add("aksh");
+        arr1.add("aksha");
+        arr1.add("akshs");
+        arr1.add("akshd");
+        arr1.add("akshgh");
+        arr1.add("akshhgh");
+        for(String item: arr1) {
+            System.out.println(item);
+        }
+        System.out.println(arr1);
+
+
+    }
+}
---
+++ src/arraylists/Arrays2.java
@@ -0,0 +1,7 @@
+package arraylists;
+
+public class Arrays2 {
+    public static void main(String[] args) {
+
+    }
+}

d273830d309f7a02ef768e3cf158cf56a30aedf4
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-18 19:38:02 +0530

    java learning arrays from prepfortech

--- src/arraylists/Arrays2.java
+++ src/arraylists/Arrays2.java
@@ -1,7 +1,22 @@
 package arraylists;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Arrays2 {
     public static void main(String[] args) {
-
+        ArrayList<String> arr1 = new ArrayList<>();
+        arr1.add("aksh");
+        arr1.add("aksha");
+        arr1.add("akshs");
+        arr1.add("akshd");
+        arr1.add("akshgh");
+        arr1.add("akshhgh");
+        Iterator<String> iter = arr1.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+            System.out.println(arr1.listIterator());
+        }
     }
 }
+

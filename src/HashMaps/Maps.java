5d4e26f2c8af398e031a773a297d892e65ec4697
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-24 18:42:51 +0530

    java learning Maps from prepfortech

--- src/HashMaps/Maps.java
+++ src/HashMaps/Maps.java
@@ -6,7 +6,7 @@ public class Maps {
     public static void main(String[] args) {
         HashMap<String, Integer> mp = new HashMap<>();
         mp.put("toyota", 178);
-        mp.put("tesla" , 200);
+        mp.put("tesla" , null);
         mp.put("toyota lo" ,85);
         mp.put("ferrari", 12);
         System.out.println(mp.size());
@@ -21,6 +21,11 @@ public class Maps {
         HashMap<String, Integer> mp2 = new HashMap<>();
         mp2.putAll(mp);
         System.out.println(mp);
+        mp2.putIfAbsent("tesla",2293);
+        System.out.println(mp2);
+        mp.replace("tesla", 22223);
+        System.out.println(mp);
+        System.out.println(mp.keySet());
 
     }
 

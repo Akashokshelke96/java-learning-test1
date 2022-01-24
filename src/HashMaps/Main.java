5a3abb8183aa1862a6add8834c8447222526d76a
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-24 18:42:34 +0530

    java learning from prepfortech

---
+++ src/HashMaps/Main.java
@@ -0,0 +1,27 @@
+package HashMaps;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        HashMap<String, Integer> ms1 = new HashMap<>();
+        ms1.put("Akash", 2000);
+        ms1.put("prakash", 23124);
+        ms1.put("savita",  232214);
+        ms1.put("shelke" , 243677);
+
+        Set<String> keys = ms1.keySet();
+        for(String key : keys) {
+            System.out.println("Key is " + key);
+        }
+        System.out.println("==============");
+        Set<Map.Entry<String, Integer>> entries = ms1.entrySet();
+        for(Map.Entry<String, Integer> entry: entries) {
+            System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
+        }
+    }
+}

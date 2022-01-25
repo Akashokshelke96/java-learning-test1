db215673c39678a665b1062f31a846fad9793fdc
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-25 11:54:43 +0530

    java learning from prepfortech try and catch

---
+++ src/Strings/StringsPractice.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class StringsPractice {
+    public static void main(String[] args) {
+        String a = "hello   G";
+        System.out.println(a.length());
+        String b = "helo";
+        if (!b.equalsIgnoreCase("helo")) {
+            System.out.println("bv");
+        }
+        else
+        {
+            System.out.println("msski");
+        }
+    }
+}

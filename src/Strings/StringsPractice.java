a5147bd99f02bb93a2ef783e83c547e3324ddc72
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-25 12:08:26 +0530

    java learning from prepfortech try and catch

--- src/Strings/StringsPractice.java
+++ src/Strings/StringsPractice.java
@@ -2,9 +2,16 @@ package Strings;
 
 public class StringsPractice {
     public static void main(String[] args) {
-        String a = "hello   G";
+        String a = "hello yogi there yogi";
         System.out.println(a.length());
         String b = "helo";
+        int l = a.indexOf("yogi", 4);
+        int k = a.lastIndexOf("yogi");
+        System.out.println(l);
+        System.out.println(k);
+
+
+
         if (!b.equalsIgnoreCase("helo")) {
             System.out.println("bv");
         }

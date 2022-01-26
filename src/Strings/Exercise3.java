6612fa21fd2dcc154a2ed77aeb145122bd2441c2
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-01-26 14:48:09 +0530

    printing String

--- src/Strings/Exercise3.java
+++ src/Strings/Exercise3.java
@@ -2,11 +2,15 @@ package Strings;
 
 public class Exercise3 {
     public static void main(String[] args) {
-        for(int i = 0; i<=100 ; i++){
-            if (i % 2 == 0){
-                System.out.println(i);
-            }
-        }// this print number zero to 100 all even
+//        for(int i = 0; i<=100 ; i++){
+//            if (i % 2 == 0){
+//                System.out.println(i);
+//            }
+//        }// this print number zero to 100 all even
+        for (int i = 0; i <= 100; i += 2) {  //this is the alternative more efficient method
+
+            System.out.println(i);
+        }
 
     }
 }

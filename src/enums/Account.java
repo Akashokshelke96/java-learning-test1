80559eb7b6c8f2fcc5325889d51219ce7e5e6993
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-01 22:46:34 +0530

    java learning enums

---
+++ src/enums/Account.java
@@ -0,0 +1,37 @@
+package enums;
+
+public class Account {
+    private double balance;
+    private String name;
+    private AccountType accountType;
+
+    public Account(double balance, String name, AccountType accountType) {
+        this.balance = balance;
+        this.name = name;
+        this.accountType = accountType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+}

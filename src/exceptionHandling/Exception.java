fa28f55f1da5c698dc3741251ca45c9eb8bfe86a
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-03 22:51:28 +0530

    java learning enums

---
+++ src/exceptionHandling/Exception.java
@@ -0,0 +1,17 @@
+package exceptionHandling;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Exception {
+    public static void main(String[] args) {
+        File file = new File("abc.txt");
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+        }
+        catch(FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+}

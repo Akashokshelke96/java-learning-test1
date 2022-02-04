d57b10eb29af639c9602fae4fd1dc74042179d27
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-04 22:44:32 +0530

    java learning enums

--- src/enums/Main.java
+++ src/enums/Main.java
@@ -7,16 +7,17 @@ public class Main {
         } catch (TransactionDeclinedException ex) {
             System.out.println(ex.getMessage());
         }
+     
 
-
-        Account account1 = new Account(10000,"akash",AccountType.savingsAccount);
-      Account account2 = new Account(12314,"pratik",AccountType.jointAccount);
-      System.out.println(account1.getAccountType());
-       System.out.println(account1.getBalance());
-       System.out.println(account1.getAccountType().getMinBalance());
+        Account account1 = new Account(10000, "akash", AccountType.savingsAccount);
+        Account account2 = new Account(12314, "pratik", AccountType.jointAccount);
+        System.out.println(account1.getAccountType());
+        System.out.println(account1.getBalance());
+        System.out.println(account1.getAccountType().getMinBalance());
         System.out.println((AccountType.currentAccount.getMinBalance())); //2nd wAY TO GET min balacne
 
     }
+
     public static void debitMoney(String account) throws TransactionDeclinedException {
         throw new TransactionDeclinedException(account + "dones not have sufficient balance!");
     }

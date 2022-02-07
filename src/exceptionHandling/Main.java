3b5266bc1440e2a3c5e8db200db98574b348e21b
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-07 23:35:59 +0530

    java learning enums

--- src/exceptionHandling/Main.java
+++ src/exceptionHandling/Main.java
@@ -1,5 +1,26 @@
 package exceptionHandling;
 
 public class Main {
+    public static void main(String[] args) {
+        try {
+            debitMoney("akash");
+        } catch (TransactionDeclinedException ex) {
+            System.out.println(ex.getMessage());
+        }
+        try {
+
+            creditMoney("nishu");
+        } catch (AccountNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void debitMoney(String account)throws TransactionDeclinedException{
+        throw new TransactionDeclinedException(account +" does not have sufficient balance");
+
+    }
+    public static void creditMoney(String account){
+        throw new AccountNotFoundException(account + " does not have enough balance!!gareeb ");
+    }
 
 }

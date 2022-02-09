8d0f9cff1a5bfe591322d93ae2088a90d2bc35eb
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-09 23:56:54 +0530

    java learning enums

---
+++ src/java1/StringLearn.java
@@ -0,0 +1,11 @@
+package java1;
+
+public class StringLearn {
+    public static void main(String[] args) {
+        String str = "akash is a very humble person";
+        for(String exmp : str.split(" ", 5 )){
+            System.out.println(exmp);
+        }
+
+    }
+}

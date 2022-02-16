19ba38949c1dcb0ce47dcb64e342d7b1ff0531b7
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-16 22:29:49 +0530

    java learning enums

--- src/exceptionHandling/Exception.java
+++ src/exceptionHandling/Exception.java
@@ -9,10 +9,16 @@ public class Exception {
         File file = new File("abc.txt");
         try {
             FileInputStream fileInputStream = new FileInputStream(file);
+
+
         }
-        catch(FileNotFoundException ex) {
+        catch(Throwable ex) {
             ex.printStackTrace();
         }
+        finally{
+            System.out.println("this code we want to execute in finally block");
+        }
+
     }
     public void debitMoney(String account) throws TransactionDeclinedException {
         throw new TransactionDeclinedException(account + " does not have enough balance!");

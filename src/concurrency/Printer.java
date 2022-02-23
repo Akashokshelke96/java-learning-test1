ec622936b6fe7e935ec90ccabaab1a9101a2e2ca
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-23 22:46:23 +0530

    java learning enums

--- src/concurrency/Printer.java
+++ src/concurrency/Printer.java
@@ -1,18 +1,26 @@
 package concurrency;
 
-public class Printer {
+public class Printer extends Thread {
+
     private int num;
     public Printer(int num){
         this.num = num;
         System.out.println("priting " + num + "!" );
     }
+    @Override
+    public void run() {
+        doPrinting();
+
+
+    }
     public void doPrinting(){
         System.out.println("i am going to sleep " + num);
         try {
-            Thread.sleep(5000);
+            Thread.sleep(5000); //sleeps for 5 seconds and then again continue
             System.out.println("i am awake now " + num);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+
 }

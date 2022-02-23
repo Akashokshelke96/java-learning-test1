095e2b7ac2771732457b5c3b658be8f289ce9402
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-23 22:46:11 +0530

    java learning enums

--- src/concurrency/Main.java
+++ src/concurrency/Main.java
@@ -1,10 +1,18 @@
 package concurrency;
 
 public class Main {
-    public static void main(String[] args) {
-        for (int i = 0 ; i < 5 ; i++){
-            Printer printer = new Printer(i);
-            printer.doPrinting();
+    public static void main(String[] args) throws InterruptedException {
+
+       // for (int i = 0 ; i < 5 ; i++){
+            Printer printer = new Printer(2);
+            printer.start();
+            Thread.sleep(500);
+            //printer.interrupt();
+
+
+            // Thread th = new Thread(new Printer(i)); // running all the threads in parallel
+           // printer.start(); // this will make the statements run in parallel
+           // printer.doPrinting(); //this will print one by one going to sleep and wake up individually
         }
     }
-}
+

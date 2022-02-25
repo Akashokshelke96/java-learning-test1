79606f90a2e9e26de9f14b970373704159c0411c
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-25 23:17:20 +0530

    initial commit multi-threading and concurrency

---
+++ src/queue/Sample.java
@@ -0,0 +1,11 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Sample {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(23);
+    }
+}

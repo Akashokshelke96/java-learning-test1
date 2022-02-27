2c512c8c7582154c640ac03614b4b2e7b8b53d1c
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-27 13:06:43 +0530

    initial commit multi-threading and concurrency

---
+++ src/queue/ArrayQueue.java
@@ -0,0 +1,5 @@
+package queue;
+
+public class ArrayQueue {// never to be used ,just for academic purposes
+
+}
--- src/queue/Sample.java
+++ src/queue/Sample.java
@@ -7,5 +7,14 @@ public class Sample {
     public static void main(String[] args) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(23);
+        queue.add(34);
+        queue.add(56);
+        System.out.println(queue.peek());
+       // queue.remove();
+        queue.poll();
+        System.out.println(queue.peek());
+        System.out.println(queue.size());
+       // System.out.println(queue.removeAll(queue));
+      //  System.out.println(queue.peek());
     }
 }

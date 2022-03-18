e29d02cae9962f6242450c644bfe73e03370dc48
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-03-18 23:06:00 +0530

    initial commit

---
+++ src/questions/Farenheit2Celsius.java
@@ -0,0 +1,18 @@
+package questions;
+
+import java.util.Scanner;
+
+public class Farenheit2Celsius {
+        public static void main(String[] args) {
+            float temperatue;
+            Scanner in = new Scanner(System.in);
+
+            System.out.println("Enter temperatue in Fahrenheit");
+            temperatue = in.nextInt();
+
+            temperatue = ((temperatue - 32)*5)/9;
+
+            System.out.println("Temperatue in Celsius = " + temperatue);
+        }
+    }
+

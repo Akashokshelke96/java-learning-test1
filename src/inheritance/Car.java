bb6a154c0f1e79c8ef43d2c74a16144278d6772a
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-03-25 22:47:13 +0530

    initial commit

--- src/inheritance/Car.java
+++ src/inheritance/Car.java
@@ -1,7 +1,7 @@
 package inheritance;
 
 public class Car {
-    public String carName;
+    public String carName;// basic example of inheritance
     public String brand;
  public void printName(String carName, String brand){
      System.out.println("My name is car" + carName + "And brand is " + brand);

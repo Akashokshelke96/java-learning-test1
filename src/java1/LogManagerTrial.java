128675ae979c8989671a6b6064a744e573fe4a3b
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-04 21:55:37 +0530

    initial commit

--- src/java1/LogManagerTrial.java
+++ src/java1/LogManagerTrial.java
@@ -8,8 +8,9 @@ public class LogManagerTrial {
             public static void main(String args[]) {
         LogManager logManager = LogManager.getLogManager();
         Logger logger = logManager.getLogger(Logger.GLOBAL_LOGGER_NAME);
-
+        logger.setLevel(Level.INFO);
         logger.log(Level.SEVERE,"this is me again log manager");
+        logger.log(Level.CONFIG,"this is finer log");
                 System.out.println("yes ");
         }
     }

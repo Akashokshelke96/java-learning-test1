856f6e338c0167f16e6c15b91154baa1a3f3366f
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-13 22:45:30 +0530

    initial commit

---
+++ src/Prep4TechQUIZ.java
@@ -0,0 +1,12 @@
+public class Prep4TechQUIZ {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 10;
+        int c = 9;
+        int d = 6;
+
+        if ((a >= b || a < c) && (b > d && !(c < d))) {
+            System.out.println("succes");
+        }
+    }
+}

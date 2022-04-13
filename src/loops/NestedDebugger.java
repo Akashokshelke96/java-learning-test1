7968513ab0b9a7b444d85957f2001a8a4f813f28
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-13 22:45:17 +0530

    initial commit

---
+++ src/loops/NestedDebugger.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class NestedDebugger {
+    public static void main(String[] args) {
+
+        for(int i = 0 ; i < 100 ; i++){
+            for(int j = 0 ; j <= 10 ; j+=2){
+                System.out.println(i + "  " + j);
+            }
+        }
+        }
+    }
+

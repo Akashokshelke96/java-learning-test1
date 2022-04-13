9e213322859d64919b498aea9408a27ff4b8a16b
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-13 22:45:42 +0530

    initial commit

---
+++ src/Strings/quiz.java
@@ -0,0 +1,21 @@
+package Strings;
+
+import javax.imageio.stream.ImageInputStream;
+
+public class quiz {
+    public static void main(String[] args) {
+        int x = 23;
+        int y = 52;
+        int z = 40;
+        int a = 64;
+
+        if((y = x + z) ==a){
+            System.out.println(--a);
+
+
+        }
+        else {
+            System.out.println(a++);
+        }
+    }
+}

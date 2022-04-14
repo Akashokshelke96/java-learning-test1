6ab6b1ee1bdd7e8b567b6e08c1460de9b729b5cb
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-14 21:51:50 +0530

    initial commit

--- src/loops/NestedDebugger.java
+++ src/loops/NestedDebugger.java
@@ -2,7 +2,7 @@ package loops;
 
 public class NestedDebugger {
     public static void main(String[] args) {
-
+//this it to understand the nested loops
         for(int i = 0 ; i < 100 ; i++){
             for(int j = 0 ; j <= 10 ; j+=2){
                 System.out.println(i + "  " + j);

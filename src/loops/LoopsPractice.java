bb46bd9b04ef13728a53961352ea4b21e349d8e8
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-14 21:51:39 +0530

    initial commit

--- src/loops/LoopsPractice.java
+++ src/loops/LoopsPractice.java
@@ -2,6 +2,7 @@ package loops;
 
 public class LoopsPractice {
     public static void main(String[] args) {
+        //understanding the break sequence
         int count = 0;
         while (count<100){
             System.out.println("Akash " + count);

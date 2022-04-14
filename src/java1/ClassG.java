f43eefe89ebae3f4dac8ab1a2d4a86f5fb86fe57
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-14 21:56:32 +0530

    initial commit

--- src/java1/ClassG.java
+++ src/java1/ClassG.java
@@ -3,7 +3,7 @@ package java1;
 public class ClassG {
     public static void main(String[] args) {
         int n = 5;
-
+//right handed start triangle pattern
         for(int i = 1; i <= n; i++) {
             int noOfSpaces = n - i;
             for (int j = 1; j <= noOfSpaces; j++){

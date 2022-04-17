56c379e7f93e6645e71b78a2d16a1f453ccadcd3
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-04-17 18:03:37 +0530

    initial commit

---
+++ src/questions/Alpha.java
@@ -0,0 +1,14 @@
+package questions;
+
+public class Alpha{
+  private double price =20.00;
+
+    public Alpha getAlpha() {
+        return alpha;
+    }
+
+    public static void setPrice(double price){
+      price = price;
+  }
+    Alpha alpha = new Alpha();
+}
